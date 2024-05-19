367f702acce29ba5d1b7c25c2fafbf43f0aa80f6
nafi-ullah <shafinnafiullah@gmail.com>
2024-05-19 10:10:56 +0600

    initial with theming

---
+++ app/[locale]/auth/page.js
@@ -0,0 +1,12 @@
+
+import style from '../page.module.css';
+
+const page = () => {
+  return (
+    <main className={style.main}>
+    <div className='flex justify-center items-center'><div className='text-3xl'>Welcome to my login page</div></div>
+    </main>
+  )
+}
+
+export default page
---
+++ app/[locale]/page.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+
+
+export default function Home() {
+  return (
+  
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+          Hello this is from jobsnavi
+          <Link href="/auth">Auth Page</Link>
+    </div>
+  );
+}
